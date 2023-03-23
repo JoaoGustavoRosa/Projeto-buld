368621722d7300d7980b570a29fe54d0ae46dd93
João Gustavo Rosa <joao.workrosa@gmail.com>
2023-03-23 17:08:42 -0300

    Iniciando o projeto
    coloquei muitas coisas

---
+++ java.js
@@ -0,0 +1,11 @@
+function clicar1(){
+    window.alert('em andamento')
+}
+
+function clicar2(){
+    window.alert('ja disse que esta em andamento')
+}
+
+function clicar3(){
+    window.alert('Irmao tu é cego?')
+}
